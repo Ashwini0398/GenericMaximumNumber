5300fe234dfce3d42a0825a2f47162a15e9e43f6
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-02-04 21:47:04 +0530

    ADDED: Check Maximum of 3 integers

--- MaximumGeneric.java
+++ MaximumGeneric.java
@@ -1,7 +1,16 @@
-public class MaximumGeneric
-{
-	public static void main(String args[])
-	{
-		System.out.println("Maximum  Number using Generic");
-	}
+public class MaximumGeneric {
+
+
+    public Integer toPrint(Integer num1, Integer num2, Integer num3) {
+        Integer maximum = num1;
+        if (num2.compareTo(maximum)>0) {
+            maximum = num2;
+        }
+        else if(num3.compareTo(maximum)>0)
+        {
+            maximum = num3;
+        }
+        return maximum;
+
+    }
 }
--- MaximumText.java
+++ MaximumText.java
@@ -1,15 +1,28 @@
-mport org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MaximumTest {
-
+    MaximumGeneric max= new MaximumGeneric();
 
     @Test
-    public void genericTests()
+    public void givenFirstNumberIsMaximum()
     {
-
-
-
+        Integer num1 = max.toPrint(24,12,10);
+        Assert.assertEquals((Integer) 24,num1);
     }
 
+    @Test
+    public void givenSecondNumberIsMaximum()
+    {
+        Integer num2 = max.toPrint(24,38,10);
+        Assert.assertEquals((Integer) 38,num2);
+    }
+
+    @Test
+    public void givenThirdNumberIsMaximum()
+    {
+        Integer num3 = max.toPrint(24,12,78);
+        Assert.assertEquals((Integer) 78,num3);
+    }
 
 }
